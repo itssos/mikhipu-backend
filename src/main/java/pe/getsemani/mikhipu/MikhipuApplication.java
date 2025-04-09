2e6bc7dffd16d9a71cf2f1bf6f75aa2afb9dfc9f
Sair Marquez <sair2003.hotmail.com@gmail.com>
2025-04-08 19:59:00 -0500

    add: base project

---
+++ src/main/java/pe/getsemani/mikhipu/MikhipuApplication.java
@@ -0,0 +1,13 @@
+package pe.getsemani.mikhipu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikhipuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikhipuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/getsemani/mikhipu/MikhipuApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.getsemani.mikhipu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhipuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
