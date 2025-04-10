114ba819b6545c8c3be18a4caef9c96fb7c8f569
Sair Marquez <sair2003.hotmail.com@gmail.com>
2025-04-09 19:22:01 -0500

    refactor: [HU001] update AuthService BadCredentialsException

--- src/main/java/pe/getsemani/mikhipu/auth/service/AuthService.java
+++ src/main/java/pe/getsemani/mikhipu/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package pe.getsemani.mikhipu.auth.service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -40,13 +41,18 @@ public class AuthService {
     }
 
     public JwtAuthResponse authenticate(LoginRequest request) {
-        var authToken = new UsernamePasswordAuthenticationToken(
-                request.getUsername(),
-                request.getPassword()
-        );
-        var authentication = authenticationManager.authenticate(authToken);
-        String jwt = tokenProvider.generateToken(authentication);
-        return new JwtAuthResponse(jwt, TOKEN_TYPE);
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(
+                    request.getUsername(),
+                    request.getPassword()
+            );
+            var authentication = authenticationManager.authenticate(authToken);
+            String jwt = tokenProvider.generateToken(authentication);
+            return new JwtAuthResponse(jwt, TOKEN_TYPE);
+
+        } catch (BadCredentialsException ex) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, ex.getMessage());
+        }
     }
 
 }
